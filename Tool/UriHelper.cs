71a1c958851f8f387539f03b94a9a48d62ac58b6
FuYin <2706117703@qq.com>
2026-07-24 18:13:14 +0800

    feat(UriHelper): 支持获取文件夹路径、文件夹的所有子文件夹路径

--- Tool/UriHelper.cs
+++ Tool/UriHelper.cs
@@ -1,18 +1,44 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
 using Windows.Storage;
 
 namespace Foldicon.Tool;
 
 public static class UriHelper
 {
+    private static readonly StorageFolder Assets =
+        Package.Current.InstalledLocation.GetFolderAsync("Assets").GetAwaiter().GetResult();
+
     /// <summary>
     /// 获取 WinUI 应用 Assets 下一个文件的路径
     /// </summary>
-    /// <param name="fileName">文件名称</param>
+    /// <param name="filePath">文件相对路径</param>
     /// <returns>绝对路径</returns>
-    public static string GetFilePathFromAssets(string fileName)
+    public static string GetFilePathFromAssets(string filePath)
     {
-        var uri = new Uri($"ms-appx:///Assets/{fileName}");
-        return (StorageFile.GetFileFromApplicationUriAsync(uri).GetAwaiter().GetResult()).Path;
+        return Assets.GetFileAsync(filePath).GetAwaiter().GetResult().Path;
+    }
+
+    /// <summary>
+    /// 获取 WinUI 应用 Assets 下一个文件夹的路径
+    /// </summary>
+    /// <param name="folderPath">文件夹相对路径</param>
+    /// <returns>绝对路径</returns>
+    public static string GetFolderPathFromAssets(string folderPath)
+    {
+        return Assets.GetFolderAsync(folderPath).GetAwaiter().GetResult().Path;
+    }
+
+    /// <summary>
+    /// 获取 WinUI 应用 Assets 下一个文件夹内，所有子文件夹的路径
+    /// </summary>
+    /// <param name="folderPath">文件夹相对路径</param>
+    /// <returns>绝对路径</returns>
+    public static string[] GetSubFolderPathFromAssets(string folderPath)
+    {
+        var path = GetFolderPathFromAssets(folderPath);
+        return Directory.GetDirectories(path);
     }
 }
