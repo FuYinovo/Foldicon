8335ccc2fac732ac231fac8c4fac1ce07f3252a3
FuYin <2706117703@qq.com>
2026-07-26 14:44:19 +0800

    style: 移除未使用的命名空间与冗余语法

--- Struct/BytesIcon.cs
+++ Struct/BytesIcon.cs
@@ -1,6 +1,6 @@
 ﻿namespace Foldicon.Struct;
 
-public record BytesIcon()
+public record BytesIcon
 {
     public string FullPath { get; set; } = string.Empty;
     public byte[]? Icon { get; set; } = [];
--- Tool/UriHelper.cs
+++ Tool/UriHelper.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.Storage;
 
